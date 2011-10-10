fd4197a40cbf9d467b6443c89caa252091d1dcfc
Joshua Clayton <jclayton@thoughtbot.com>
2011-10-10 01:19:29 -0400

    Columnize output

--- lib/scarab/cli.rb
+++ lib/scarab/cli.rb
@@ -1,7 +1,17 @@
+require "active_support/core_ext/array/grouping"
+
 module Scarab
   class Cli
     def initialize(*args)
-      puts WordList.new(args.first).join("\n")
+      list = WordList.new(args.first).to_a
+      longest = list.sort_by(&:length).reverse.first.length
+
+      terminal_width = Integer(`tput cols`.strip)
+      columns = terminal_width/(longest + 4)
+
+      list.in_groups_of(columns).each do |row|
+        puts row.map {|word| sprintf("%-#{longest + 4}s", word) }.join
+      end
     end
   end
 end
